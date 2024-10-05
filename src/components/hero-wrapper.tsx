c77833a92a6c96e369f4342b31bdc4f776215e9b
Vardan Galstyan <vardan.galstjan@gmail.com>
2024-10-05 21:45:17 +0200

    feat: adding react scroll parallax

--- src/components/hero-wrapper.tsx
+++ src/components/hero-wrapper.tsx
@@ -1,14 +1,37 @@
+"use client";
+
+import Image from "next/image";
+import { ParallaxProvider, useParallax } from "react-scroll-parallax";
+
 const HeroWrapper = () => {
-  const imageUrl =
-    "https://www.seasidestartupsummit.com/assets/media/bg.6ca3fdd4.jpg";
+  return (
+    <div className="relative overflow-hidden h-[90vh]">
+      <ParallaxProvider>
+        <TestParallax />
+        <section className="absolute h-full w-full flex justify-center items-center text-white">
+          Hero Section
+        </section>
+      </ParallaxProvider>
+    </div>
+  );
+};
+
+export const TestParallax = () => {
+  const { ref } = useParallax<HTMLDivElement>({
+    speed: -50,
+    translateY: [-111.3, 100],
+  });
 
   return (
-    <section
-      style={{ backgroundImage: `url(${imageUrl})` }}
-      className="h-[90vh] bg-cover bg-fixed bg-center bg-no-repeat w-full"
-    >
-      Hero Section
-    </section>
+    <div ref={ref} className="absolute w-full h-full">
+      <Image
+        className="object-cover"
+        alt="Image"
+        sizes="100vw, 50vw, 33vw"
+        fill
+        src="https://www.seasidestartupsummit.com/assets/media/bg.6ca3fdd4.jpg"
+      />
+    </div>
   );
 };
 
